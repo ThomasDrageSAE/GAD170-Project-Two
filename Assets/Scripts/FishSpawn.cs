ffc8f39f629753c2bbc11a9b4cf7fedd42d483c9
DESKTOP-18177EO\Thomas <1034828@student.sae.edu.au>
2025-03-13 02:20:06 +0800

    Edited scene and assets

--- Assets/Scripts/FishSpawn.cs
+++ Assets/Scripts/FishSpawn.cs
@@ -19,9 +19,9 @@ public class FishSpawn : MonoBehaviour
 
     public void SpawnFish()
     {
-        float x = Random.Range(0, 50);
+        float x = Random.Range(0, 100);
         float y = 1.67f;
-        float z = Random.Range(0, 50);
+        float z = Random.Range(0, 100);
         Instantiate(fishPrefab,new Vector3(x,y,z), Quaternion.identity);
     }
 }
